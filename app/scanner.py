e9594cae553ffb826a8701ec949b0e03187afc19
Cse18107 <cse18107@cemk.ac.in>
2025-08-04 07:53:09 +0530

    added new chagnes

--- app/scanner.py
+++ app/scanner.py
@@ -146,7 +146,6 @@ def _get_input_scanner(
         scanner_config["model"] = BAN_COMPETITORS_MODEL
 
     if scanner_name == "Code":
-        CODE_MODEL.kwargs["low_cpu_mem_usage"] = True
         _configure_model(CODE_MODEL, scanner_config)
         scanner_config["model"] = CODE_MODEL
 
@@ -224,7 +223,6 @@ def _get_output_scanner(
         scanner_config["model"] = BIAS_MODEL
 
     if scanner_name == "Code":
-        CODE_MODEL.kwargs["low_cpu_mem_usage"] = True
         _configure_model(CODE_MODEL, scanner_config)
         scanner_config["model"] = CODE_MODEL
 
